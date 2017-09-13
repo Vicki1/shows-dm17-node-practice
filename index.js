d18775e29c5f0db82f7bea04f196f8cbcce1cfc0
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2017-09-12 19:21:47 -0600

    in progress

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var bodyParser=require('body-parser');
 var showsController = require('./controllers/shows_controllers.js');
 
 
-var app = express();
-
-app.use(bodyParser.json());
-// could give ^ a path but we don't because we want it to run with every request
-app.use(function(req,res,next){
+var logQuery = function(req,res,next){
     console.log(req.path);
     console.log(req.query);
+    //  or could use function to verify the user, get me the user object, so can use user object to give them specific stuff
     next(); //=> next() makes us able to run code further down on the page
-})
+}
+
+var app = express();
+//app.use if want it called with every request??
+app.use(bodyParser.json());
+// could give ^ a path but we don't because we want it to run with every request
+app.use(logQuery);
+
 function test(req, res, next){
     console.log('puppy');
     next();
@@ -20,10 +24,10 @@ function test(req, res, next){
 //and the stuff after the question mark is added to an object;
 //   ex:  /shows?genre=comedy&rating=9 
 //    ^ {genre: 'comedy', rating: '9'} this object is saved to req.query
-app.get('/shows', test, showsController.index );
+app.get('/shows', test, logQuery, showsController.index );
 app.get('shows/last', showsController.last);
 app.get('/shows/:id', showsController.show );
-app.post('/shows', showsController.create );
+app.post('/shows', logQuery, showsController.create );
 app.delete('/shows/:id', showsController.destroy);
 
 
